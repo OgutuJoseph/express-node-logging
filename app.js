1547d401365287cc553cc58cb226fbd61bba8840
OgutuJoseph <onyango.joseph@yahoo.com>
2023-01-10 11:24:28 +0300

    Default route testing

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+const expressWinston = require('express-winston');
+const { transports, format } = require('winston');
 
+/** port */
 const port = process.env.PORT;
+
+app.use(expressWinston.logger({
+    transports: [
+        new transports.Console()
+    ],
+    format: format.combine(
+        format.json(),
+        format.timestamp(),
+        format.prettyPrint()
+    )
+}));
 
 app.get('/', (req, res) => {
     res.sendStatus(200);
